494add293953072c0d2650377ca477789555773b
minli <user@g-klin.ru>
2026-04-05 17:42:15 +0300

    Add static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/g-klin",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
